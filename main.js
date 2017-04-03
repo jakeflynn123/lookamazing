83d790696ceccf31d86bf35e25a0e191496ad871
Jacob <jakeflynn.yt@gmail.com>
2017-04-03 18:29:09 +0100

    few changes to fix forms

--- main.js
+++ main.js
@@ -5,25 +5,18 @@ $(function() {
         $("#creditcardform").hide();
         $('#paypalform').show();
         $('#alipayform').hide();
-        $('#findpostcode').hide();
-        $('#appendEnterAddress').hide();
-        $('#enteraddressmanually').hide();
     });
 
     $("#creditcard").on('click', function () {
         $("#paypalform").hide();
         $('#creditcardform').show();
         $('#alipayform').hide();
-        $('#findpostcode').hide();
-        $('#appendEnterAddress').hide();
+
     });
     $("#alipay").on('click', function () {
         $("#paypalform").hide();
         $('#creditcardform').hide();
         $('#alipayform').show();
-        $('#findpostcode').hide();
-        $('#appendEnterAddress').hide();
-        $('#enteraddressmanually').hide();
     });
     $("#addressbutton").on('click', function(){
         var postcode = document.getElementById("smallinput").value;
@@ -39,6 +32,7 @@ $(function() {
     $("#enteraddress").on('click', function() {
         $('#findpostcode').show();
         $("#enteraddressmanually").show();
+        $("#address").hide();
     });
 
     $('#noaddress').on('click', function () {
@@ -48,25 +42,38 @@ $(function() {
 
     $('#submitorder').on('click', function() {
         if(document.getElementById("creditcard").checked) {
-            var x = document.forms["myForm"]["cardnumber"].value;
-            if (x == "") {
-                window.alert("Fields must be filled out");
-                return false;
+            var x = [document.forms["paymentform"]["cardnumber"].value, document.forms["paymentform"]["cv2"].value, document.forms["paymentform"]["expirydate"].value, document.forms["paymentform"]["nameoncard"].value];
+            for (var i = 0; i < x.length; i++) {
+                if (x[i] == "") {
+                    window.alert("Please Enter Your Credit Card Details");
+                    return false;
+                }
             }
-        }else if(document.getElementById("paypal").checked){
-            return none;
-        }else if(document.getElementById("alipay").checked) {
-            return none;
-        }else{
-            window.alert("Select an payment option");
-        }
-        });
-});
+            } else if (document.getElementById("paypal").checked) {
+                return addressCheck();
+            } else if (document.getElementById("alipay").checked) {
+                return addressCheck();
+            } else {
+                window.alert("Select an payment option");
+            }
 
+    });
+});
+function addressCheck() {
+    if(document.getElementById("enteraddress").checked) {
+        var postcode = document.forms["postcode"]["postcode"].value;
+        if (postcode = "") {
+            window.alert("Please Enter a Postcode/ZipCode");
+        }
+    }else{
+        window.alert("Thank you! your order is complete, payment shall follow shortly")
+    }
+}
 $(function () {
     $("#enteraddressmanually").one('click', function () {
         $("#appendEnterAddress").show();
         $(this).hide();
+        $('#addressbutton').hide();
     });
 });
 
@@ -74,4 +81,20 @@ $(function () {
    $('#alipaybutton').on('click', function () {
     $('#alipayform').hide();
    });
+});
+
+$('#saveaddress').on('click', function() {
+    var o = [document.forms["address"]["housenumber"].value, document.forms["postcode"]["postcode"].value,
+        document.forms["address"]["addressone"].value, document.forms["address"]["TownCity"].value];
+    for (var h = 0; h < o.length; h++) {
+        if (o[h] == "") {
+            window.alert("Please Enter Your Address or Select Use The Same Address");
+            return false;
+        } else {
+            $("#saved").show();
+            $("#save").hide();
+            $("#saveaddress").css('background-color', '#28bdb3');
+            $("#saveaddress").css('color', 'white');
+        }
+    }
 });
